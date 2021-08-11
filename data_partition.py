eaf543af8b57f7bc31ed4206e4f7695305c7eb2f
Albert Fu <81748574+laofuge@users.noreply.github.com>
2021-08-11 17:56:48 +0800

    Update data_partition.py

--- data_partition.py
+++ data_partition.py
@@ -66,73 +66,15 @@ with open('../data/train2.txt','r',encoding='utf-8') as f:
 f_val_validation2.close()
 f_val_train2.close()
 
-#需要在列间插入“<sssss>”
-# list_399300=[]
-# with open('../data/index_train.txt', 'r', encoding='UTF-8') as f:
-# 	k=0
-# 	for line in f:
-# 		if k<100:
-# 			tokens = line.strip().split('     ')[1]
-# 			#print(tokens)
-# 			list_399300.append(tokens.strip())
-# 		elif k<1000:
-# 			tokens = line.strip().split('    ')[1]
-# 			#print(tokens)
-# 			list_399300.append(tokens.strip())
-# 		else:
-# 			tokens = line.strip().split('   ')[1]
-# 			#print(tokens)
-# 			list_399300.append(tokens.strip())
-# 		k+=1
-# #print(list_399300)
-# list_399300.reverse()
-
-# del list_399300[0]
-
-# del list_399300[-1]
-# # print(list_399300)  #未删除日期，一个元素5个数，数中间两个空格，组成的列表
-
-# # print(float(list_399300[60].split('  ')[1]))
-
-
-# f=open('../data/new_index_train.txt','w',encoding='utf-8')
-
-# for i in range(len(list_399300)-60):
-# 	t_list=list_399300[i:i+60]
-# 	t_label=str(float(list_399300[i+60].split('  ')[1])/float(list_399300[i].split('  ')[1])-1)
-# 	# print(t_label)
-# 	seq='<sssss>'.join(t_list)
-# 	#f.write(seq+'\n')
-# 	seqs='\t\t'.join(['1','1',t_label,seq])
-# 	f.write(seqs+'\n')
-# f.close()
-
-# # embeddings=[]
-# # for sentence in list_399300:
-# # 	words=sentence.split('  ')[1:]
-# # 	embeddings.append([float(word) for word in words]) #转为float没有引号 words是一个列表所以加[]
-# # # print(embeddings)
-
-# # import pickle as pk
-# # new_embeddings=np.array(embeddings) #改为矩阵形式
-# # pk.dump(new_embeddings, open('new_embeddings.save', 'wb')) #保存
-
-
-# # f=open('date.txt','w',encoding='utf-8')
-# # for all_date in list_399300:
-# # 	date=all_date.split('  ')[0]
-# # 	f.write(date+'\n')
-# # f.close()
-
-# #建立文档：第四列只含日期，中间<sssss>隔开
-# # f=open('../data/new_index_train.txt','w',encoding='utf-8')
-# # for i in range(len(list_399300)-60):
-# # 	t_list=[date.split('  ')[0] for date in list_399300[i:i+60]]
-# # 	t_label=str(float(list_399300[i+60].split('  ')[1])/float(list_399300[i].split('  ')[1])-1)
-# # 	seq='<sssss>'.join(t_list)
-# # 	#f.write(seq+'\n')
-# # 	seqs='\t\t'.join(['1','1',t_label,seq])
-# # 	f.write(seqs+'\n')
-# # f.close()
-
-
+f_trainlabel=open('../data/trainlabel.txt','w',encoding='utf-8')
+f_testlabel=open('../data/testlabel.txt','w',encoding='utf-8')
+with open('../data/labels.txt','r',encoding='UTF-8') as f:
+	k=0
+	for line in f:
+		if 78800:
+			f_trainlabel.write(line)
+		else:
+			f_testlabel.write(line)
+		k+=1
+f_testlabel.close()
+f_trainlabel.close()
